8826ea1242f5f20318e8d97e65958833860b9a28
taher19999 <94671425+taher19999@users.noreply.github.com>
2023-04-06 21:18:55 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,11 +5,11 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+with open(config_dir / "config/config.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
 # load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
+config_env = dotenv.dotenv_values(config_dir / "config/config.env")
 
 # config parameters
 telegram_token = config_yaml["telegram_token"]
@@ -21,9 +21,9 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 
 # chat_modes
-with open(config_dir / "chat_modes.yml", 'r') as f:
+with open(config_dir / "config/chat_modes.yml", 'r') as f:
     chat_modes = yaml.safe_load(f)
 
 # models
-with open(config_dir / "models.yml", 'r') as f:
+with open(config_dir / "config/models.yml", 'r') as f:
     models = yaml.safe_load(f)
